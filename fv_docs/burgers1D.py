fixed indexing on cell boundary value calculation in piecewise linear TVD reconstructions. These are now actually TVD and stable

fv_docs/burgers1D.py
@@ -24,7 +24,7 @@ ufunc = mth.BurgersWave1D()
 
 #set up problem
 nx = 201+2
-nt = 1000
+nt = 1600
 L  = 2.*np.pi
 dx = L/(nx-1)
 nu = 0.17